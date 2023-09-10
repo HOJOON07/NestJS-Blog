21eda2031d52905257b2dc66c8841fd52e96281e
HOJOON07 <ghwns107@naver.com>
2023-09-11 00:52:22 +0900

    게시글 기본 api 구현

--- src/blog.service.ts
+++ src/blog.service.ts
@@ -1,1 +1,34 @@
-export class BlogService {}
+import { PostDto } from './blog.model';
+
+export class BlogService {
+  posts = [];
+
+  getAllPosts() {
+    return this.posts;
+  }
+
+  createPost(postDto: PostDto) {
+    const id = this.posts.length + 1;
+    this.posts.push({ id: id.toString(), ...postDto, createdDt: new Date() });
+  }
+
+  getPost(id: string) {
+    const post = this.posts.find((post) => {
+      return post.id === id;
+    });
+    console.log(post);
+    return post;
+  }
+
+  delete(id: string) {
+    const filterdPosts = this.posts.filter((post) => post.id !== id);
+    this.posts = [...filterdPosts];
+  }
+
+  updatePost(id, postDto: PostDto) {
+    const updateIndex = this.posts.findIndex((post) => post.id !== id);
+    const updatePost = { id, ...postDto, updatedDt: new Date() };
+    this.posts[updateIndex] = updatePost;
+    return updatePost;
+  }
+}
